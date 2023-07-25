b0047f231d5782a18e58ce7f14dfbe5a1774e127
Tobias Theuer <tobias.theuer@gmx.de>
2023-07-25 08:01:14 +0200

    Large refactoring: Fix huge missed optimization opportunity by not updating alpha between top level child node explorations, cacnel search when time is short, use ints instead of floats for scores everywhere, fix board center recognition (again, this time for real), reduce tokens needed for minimax search. Add simple negamax implementation from github to EvilBot. Our bot has at least a 300 Elo points higher rating than EvilBot atm

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -8,124 +8,140 @@ namespace ChessChallenge.Example
     // Plays randomly otherwise.
     public class EvilBot : IChessBot
     {
+     
+        //                     .  P    K    B    R    Q    K
+        int[] kPieceValues = { 0, 100, 300, 310, 500, 900, 10000 };
+        int kMassiveNum = 99999999;
 
-        Board b;
+        int mDepth;
+        Move mBestMove;
 
         public Move Think(Board board, Timer timer)
         {
-            b = board;
+            Move[] legalMoves = board.GetLegalMoves();
+            mDepth = 3;
 
-            var moves = board.GetLegalMoves();
-            var bestMove = moves[0];
-            var bestScore = float.NegativeInfinity;
-            foreach (var move in moves)
+            EvaluateBoardNegaMax(board, mDepth, -kMassiveNum, kMassiveNum, board.IsWhiteToMove ? 1 : -1);
+
+            return mBestMove;
+        }
+
+        int EvaluateBoardNegaMax(Board board, int depth, int alpha, int beta, int color)
+        {
+            Move[] legalMoves;
+
+            if (board.IsDraw())
+                return 0;
+
+            if (depth == 0 || (legalMoves = board.GetLegalMoves()).Length == 0)
             {
-                var result = -deepen(move, 3, float.NegativeInfinity, float.PositiveInfinity);
-                if (result > bestScore)
+                // EVALUATE
+                int sum = 0;
+
+                if (board.IsInCheckmate())
+                    return -9999999;
+
+                for (int i = 0; ++i < 7;)
+                    sum += (board.GetPieceList((PieceType)i, true).Count - board.GetPieceList((PieceType)i, false).Count) * kPieceValues[i];
+                // EVALUATE
+
+                return color * sum;
+            }
+
+            // TREE SEARCH
+            int recordEval = int.MinValue;
+            foreach (Move move in legalMoves)
+            {
+                board.MakeMove(move);
+                int evaluation = -EvaluateBoardNegaMax(board, depth - 1, -beta, -alpha, -color);
+                board.UndoMove(move);
+
+                if (recordEval < evaluation)
                 {
-                    bestMove = move;
-                    bestScore = result;
-                    Console.WriteLine("move: " + bestMove.ToString());
+                    recordEval = evaluation;
+                    if (depth == mDepth)
+                        mBestMove = move;
                 }
+                alpha = Math.Max(alpha, recordEval);
+                if (alpha >= beta) break;
             }
+            // TREE SEARCH
 
-            Console.WriteLine("Score: " + bestScore);
-            return bestMove;
+            return recordEval;
         }
+        //Board b;
 
-        // return the score from the point of view of the current player, i.e. a high value
-        // means that the player who can choose between `moves` thinks they are winning
-        float search_score(Move[] moves, int depth, float alpha, float beta)
-        {
-            foreach (var move in moves)
-            {
-                var result = -deepen(move, depth, alpha, beta);
-                if (result >= beta) return beta;
-                alpha = Math.Max(alpha, result);
-            }
+        //public Move Think(Board board, Timer timer)
+        //{
+        //    b = board;
 
-            return alpha;
-        }
-
-        // return the score from the point of view of the player who can move after `move` has been made,
-        // ie a high value means that `move` was probably a bad move because the opponent likes their position
-        float deepen(Move move, int depth, float alpha, float beta)
-        {
-            var score = float.NegativeInfinity;
-            b.MakeMove(move);
-
-            if (b.GetLegalMoves().Length == 0)
-            {
-                if (!b.IsInCheck()) score = 0;
-            }
-            else if (depth <= 0)
-            {
-                /*if (depth > -3)
-                {
-                    var captures = b.GetLegalMoves(true);
-                    if (captures.Length > 0) score = Math.Max(alpha, -search_score(captures, depth - 1, -beta, -alpha));
-                    else score = eval();
-
-                }
-                else*/
-                score = eval();
-            }
-            else
-            {
-                score = search_score(b.GetLegalMoves(), depth - 1, -beta, -alpha);
-            }
-            b.UndoMove(move);
-            return score;
-        }
-
-        int eval()
-        {
-            return b.GetAllPieceLists().Select(pieceList =>
-                pieceList.Count * (int)pieceList.TypeOfPieceInList * (pieceList.IsWhitePieceList == b.IsWhiteToMove ? 1 : -1)).Sum();
-        }
-        //    // Piece values: null, pawn, knight, bishop, rook, queen, king
-        //    int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
-
-        //    public Move Think(Board board, Timer timer)
+        //    var moves = board.GetLegalMoves();
+        //    var bestMove = moves[0];
+        //    var bestScore = float.NegativeInfinity;
+        //    foreach (var move in moves)
         //    {
-        //        Move[] allMoves = board.GetLegalMoves();
-
-        //        // Pick a random move to play if nothing better is found
-        //        Random rng = new();
-        //        Move moveToPlay = allMoves[rng.Next(allMoves.Length)];
-        //        int highestValueCapture = 0;
-
-        //        foreach (Move move in allMoves)
+        //        var result = -deepen(move, 3, float.NegativeInfinity, float.PositiveInfinity);
+        //        if (result > bestScore)
         //        {
-        //            // Always play checkmate in one
-        //            if (MoveIsCheckmate(board, move))
-        //            {
-        //                moveToPlay = move;
-        //                break;
-        //            }
-
-        //            // Find highest value capture
-        //            Piece capturedPiece = board.GetPiece(move.TargetSquare);
-        //            int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
-
-        //            if (capturedPieceValue > highestValueCapture)
-        //            {
-        //                moveToPlay = move;
-        //                highestValueCapture = capturedPieceValue;
-        //            }
+        //            bestMove = move;
+        //            bestScore = result;
+        //            Console.WriteLine("move: " + bestMove.ToString());
         //        }
-
-        //        return moveToPlay;
         //    }
 
-        //    // Test if this move gives checkmate
-        //    bool MoveIsCheckmate(Board board, Move move)
+        //    Console.WriteLine("Score: " + bestScore);
+        //    return bestMove;
+        //}
+
+        //// return the score from the point of view of the current player, i.e. a high value
+        //// means that the player who can choose between `moves` thinks they are winning
+        //float search_score(Move[] moves, int depth, float alpha, float beta)
+        //{
+        //    foreach (var move in moves)
         //    {
-        //        board.MakeMove(move);
-        //        bool isMate = board.IsInCheckmate();
-        //        board.UndoMove(move);
-        //        return isMate;
+        //        var result = -deepen(move, depth, alpha, beta);
+        //        if (result >= beta) return beta;
+        //        alpha = Math.Max(alpha, result);
         //    }
+
+        //    return alpha;
+        //}
+
+        //// return the score from the point of view of the player who can move after `move` has been made,
+        //// ie a high value means that `move` was probably a bad move because the opponent likes their position
+        //float deepen(Move move, int depth, float alpha, float beta)
+        //{
+        //    var score = float.NegativeInfinity;
+        //    b.MakeMove(move);
+
+        //    if (b.GetLegalMoves().Length == 0)
+        //    {
+        //        if (!b.IsInCheck()) score = 0;
+        //    }
+        //    else if (depth <= 0)
+        //    {
+        //        /*if (depth > -3)
+        //        {
+        //            var captures = b.GetLegalMoves(true);
+        //            if (captures.Length > 0) score = Math.Max(alpha, -search_score(captures, depth - 1, -beta, -alpha));
+        //            else score = eval();
+
+        //        }
+        //        else*/
+        //        score = eval();
+        //    }
+        //    else
+        //    {
+        //        score = search_score(b.GetLegalMoves(), depth - 1, -beta, -alpha);
+        //    }
+        //    b.UndoMove(move);
+        //    return score;
+        //}
+
+        //int eval()
+        //{
+        //    return b.GetAllPieceLists().Select(pieceList =>
+        //        pieceList.Count * (int)pieceList.TypeOfPieceInList * (pieceList.IsWhitePieceList == b.IsWhiteToMove ? 1 : -1)).Sum();
         //}
     }
 }
--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -5,6 +5,7 @@ using ChessChallenge.API;
 public class MyBot : IChessBot
 {
     Board b;
+    // piece values from stockfish's middle game evaluation function. Our bot won't make it to even endgames
     private int[] pieceValues = { 0, 126, 781, 825, 1276, 2538, 0 };
 
 
@@ -14,77 +15,76 @@ public class MyBot : IChessBot
 
         var moves = board.GetLegalMoves();
         var bestMove = moves[0];
-        var bestScore = float.NegativeInfinity;
-        foreach (var move in moves)
-        {
-            var result = -deepen(move, 3, float.NegativeInfinity, float.PositiveInfinity);
-            if (result > bestScore)
+        var bestScore = -1_000_000;
+        //for (int depth = 1; depth < 4; ++depth) // TODO: Actually use iterative deepening for something
+        //{
+            var depth = 3;
+            foreach (var move in moves)
             {
-                bestMove = move;
-                bestScore = result;
-            }
-        }
+                b.MakeMove(move);
+                var result = -negamax(depth, -1_000_000, -bestScore);
+                b.UndoMove(move);
+                if (result > bestScore)
+                {
+                    bestMove = move;
+                    bestScore = result;
+                }
+                if (timer.MillisecondsElapsedThisTurn * 8 > timer.MillisecondsRemaining) return bestMove;
+            }   
+        //}
         return bestMove;
     }
 
-    // return the score from the point of view of the current player, i.e. a high value
-    // means that the player who can choose between `moves` thinks they are winning
-    float search_score(Move[] moves, int depth, float alpha, float beta)
+    // return the score from the point of view of the player who can move now,
+    // ie a high value means that the current playerlikes their position
+    int negamax(int depth, int alpha, int beta)
     {
-        foreach (var move in moves)
-        {
-            var result = -deepen(move, depth, alpha, beta);
-            if (result >= beta) return beta;
-            alpha = Math.Max(alpha, result);
-        }
 
+        if (b.IsInCheckmate())
+            return -1_000_000;
+        else if (b.IsDraw())
+            return 0;
+        else if (depth <= 0)
+            return eval();
+        foreach (var move in b.GetLegalMoves())
+        {
+            b.MakeMove(move);
+            alpha = Math.Max(alpha, -negamax(depth - 1, -beta, -alpha));
+            b.UndoMove(move);
+            if (alpha >= beta) return alpha;
+        }
         return alpha;
     }
 
-    // return the score from the point of view of the player who can move after `move` has been made,
-    // ie a high value means that `move` was probably a bad move because the opponent likes their position
-    float deepen(Move move, int depth, float alpha, float beta)
+    int eval()
     {
-        var score = float.NegativeInfinity;
-        b.MakeMove(move);
-
-        if (b.IsInCheckmate()) {
-        } else if (b.IsDraw()) {
-            score = -1;
-        } else if (depth <= 0) {
-            /*if (depth > -3)
-            {
-                var captures = b.GetLegalMoves(true);
-                if (captures.Length > 0) score = Math.Max(alpha, -search_score(captures, depth - 1, -beta, -alpha));
-                else score = eval();
-                
-            }
-            else*/ score = eval();
-        } else {
-            score = search_score(b.GetLegalMoves(), depth - 1, -beta, -alpha);
-        }
-        b.UndoMove(move);
-        return score;
-    }
-
-    int eval() {
         return evalPlayer(b.IsWhiteToMove) - evalPlayer(!b.IsWhiteToMove);
     }
 
-    int evalPlayer(bool color) {
+
+    int evalPlayer(bool color, bool debug = false)
+    {
         int material = b.GetAllPieceLists().Select(pieceList =>
                 pieceList.Count * pieceValues[(int)pieceList.TypeOfPieceInList] * (pieceList.IsWhitePieceList == color ? 1 : 0)).Sum();
-        int position = Math.Abs(b.GetKingSquare(color).Rank - 4) * (material - 2000) / 100 // total material 9310, so go to the center in the endgame
+
+        int position = Math.Abs(b.GetKingSquare(color).Rank - 4) * (material - 2000) / 100 // total material is 9310, so go to the center in the endgame
             + b.GetPieceList(PieceType.Knight, color).Select(
                 knight => BitboardHelper.GetNumberOfSetBits(BitboardHelper.GetKnightAttacks(knight.Square))).Sum() // how well are knights placed?
             + b.GetPieceList(PieceType.Pawn, color).Select(pawn => Math.Abs((color ? 7 : 0) - pawn.Square.Rank)).Sum() // advancing pawns is good
-            + BitboardHelper.GetNumberOfSetBits((color ? b.WhitePiecesBitboard : b.BlackPiecesBitboard) & 0x00c3c3c3c300); // controlling the center is good
+            // controlling the center is good, as is having pieces slightly forward
+            + BitboardHelper.GetNumberOfSetBits(color ? b.WhitePiecesBitboard & 0x003c_3c3c_3c3c_0000 : b.BlackPiecesBitboard & 0x0000_3c3c_3c3c_3c00);
+
         for (int slidingPiece = 3; slidingPiece <= 5; ++slidingPiece)
         {
-            position += b.GetPieceList((PieceType)slidingPiece, color).Select( // how well are sliding pieces placed?
-                piece => BitboardHelper.GetNumberOfSetBits(BitboardHelper.GetSliderAttacks((PieceType)slidingPiece, piece.Square, b))).Sum();
+            position += b.GetPieceList((PieceType)slidingPiece, color).Select( piece => // how well are sliding pieces placed?
+                BitboardHelper.GetNumberOfSetBits(BitboardHelper.GetSliderAttacks((PieceType)slidingPiece, piece.Square, b))).Sum();
         }
-        // choosing custom factors (including for the different summmands of `position` may improve this evaluation, but this already seems relatively decent
+        if (debug)
+        {
+            Console.WriteLine("material: " + material.ToString() + ", position: " + position);
+        }
+
+        // choosing custom factors (including for the different summmands of `position`) may improve this evaluation, but this already seems relatively decent
         return material + position;
     }
 
