10a18436ffc40e7685beeed794aeb3f8e47954ae
Tobias Theuer <tobias.theuer@gmx.de>
2023-08-07 15:26:45 +0200

    Add nps debug output to evil bot

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -1,4 +1,9 @@
-﻿using ChessChallenge.API;
+﻿#if DEBUG
+// comment this to stop printing debug/benchmarking information
+#define PRINT_DEBUG_INFO
+#endif
+
+using ChessChallenge.API;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -349,6 +354,10 @@ namespace ChessChallenge.Example
         int[] piecePhase = { 0, 0, 1, 1, 2, 4, 0 };
         ulong[] psts = { 657614902731556116, 420894446315227099, 384592972471695068, 312245244820264086, 364876803783607569, 366006824779723922, 366006826859316500, 786039115310605588, 421220596516513823, 366011295806342421, 366006826859316436, 366006896669578452, 162218943720801556, 440575073001255824, 657087419459913430, 402634039558223453, 347425219986941203, 365698755348489557, 311382605788951956, 147850316371514514, 329107007234708689, 402598430990222677, 402611905376114006, 329415149680141460, 257053881053295759, 291134268204721362, 492947507967247313, 367159395376767958, 384021229732455700, 384307098409076181, 402035762391246293, 328847661003244824, 365712019230110867, 366002427738801364, 384307168185238804, 347996828560606484, 329692156834174227, 365439338182165780, 386018218798040211, 456959123538409047, 347157285952386452, 365711880701965780, 365997890021704981, 221896035722130452, 384289231362147538, 384307167128540502, 366006826859320596, 366006826876093716, 366002360093332756, 366006824694793492, 347992428333053139, 457508666683233428, 329723156783776785, 329401687190893908, 366002356855326100, 366288301819245844, 329978030930875600, 420621693221156179, 422042614449657239, 384602117564867863, 419505151144195476, 366274972473194070, 329406075454444949, 275354286769374224, 366855645423297932, 329991151972070674, 311105941360174354, 256772197720318995, 365993560693875923, 258219435335676691, 383730812414424149, 384601907111998612, 401758895947998613, 420612834953622999, 402607438610388375, 329978099633296596, 67159620133902 };
 
+        #if PRINT_DEBUG_INFO
+        private int nodeCtr;
+        #endif
+        
         // https://www.chessprogramming.org/Transposition_Table
         struct TTEntry
         {
@@ -404,6 +413,10 @@ namespace ChessChallenge.Example
             bool notRoot = ply > 0;
             int best = -30000;
 
+#if PRINT_DEBUG_INFO
+            ++nodeCtr;
+#endif
+            
             // Check for repetition (this is much more important than material and 50 move rule draws)
             if (notRoot && board.IsRepeatedPosition())
                 return 0;
@@ -491,6 +504,9 @@ namespace ChessChallenge.Example
 
         public Move Think(Board board, Timer timer)
         {
+#if PRINT_DEBUG_INFO
+            nodeCtr = 0;
+#endif
             // https://www.chessprogramming.org/Iterative_Deepening
             for (int depth = 1; depth <= 50; depth++)
             {
@@ -500,6 +516,9 @@ namespace ChessChallenge.Example
                 if (timer.MillisecondsElapsedThisTurn >= timer.MillisecondsRemaining / 30)
                     break;
             }
+#if PRINT_DEBUG_INFO
+            Console.WriteLine("Evil Bot: Nodes: {0}, NPS: {1}k", nodeCtr, (nodeCtr / (double)timer.MillisecondsElapsedThisTurn).ToString("0.0"));
+#endif
             return bestmoveRoot;
         }
     }
